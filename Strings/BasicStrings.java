ce7de2359e54e6ea365382c5e03e5e3075408992
Tiru2002 <tirumalarao1805@gmail.com>
2024-03-10 13:59:32 +0530

    update

---
+++ Strings/BasicStrings.java
@@ -0,0 +1,13 @@
+package Strings;
+
+public class BasicStrings {
+    public static void main(String[] args) {
+        char ch[]={'H','e','l','l','o'};
+        String str=new String(ch); //Converting character array to string .
+        System.out.println("The given characters are : "+str);
+
+        System.out.println("length of string is "+str.length() );
+        
+    
+    }
+}
