5b9f37c4a0b2fef842c05470bf26e1804ff90b9f
Tiru2002 <tirumalarao1805@gmail.com>
2024-03-12 22:16:32 +0530

    update

--- Introduction/TypeCasting/ExplicitCasting.java
+++ Introduction/TypeCasting/ExplicitCasting.java
@@ -7,6 +7,7 @@ public class ExplicitCasting {
         double d=100.09;
         int i=(int)d;//Explicit Casting - Type casting is the process\n" + 
         System.out.println("int: "+i);
+        
         short s=(short)i; //It is used to convert a variable
         System.out.println("Short :"+s);
     }
